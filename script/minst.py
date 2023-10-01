db2e1254e3dce8a3908ca1e63d49421e403eeb6a
ksks2012 <st890751999@gmail.com>
2023-10-01 15:19:06 +0800

    feat(minst): Add recognize_number to input other number image

--- script/minst.py
+++ script/minst.py
@@ -8,6 +8,7 @@ from torch import nn
 
 PATH = "./ai_model/model_minst"
 
+
 class CNN(torch.nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
@@ -15,35 +16,39 @@ class CNN(torch.nn.Module):
         self.conv2 = nn.Conv2d(16, 32, kernel_size=(3, 3))
         self.maxpool = nn.MaxPool2d(kernel_size=(2, 2))
         self.lin1 = nn.Linear(800, 128)
-        self.out = nn.Linear(128, 10) # flatten output of CNN
+        self.out = nn.Linear(128, 10)  # flatten output of CNN
 
-    def forward(self,x):
+    def forward(self, x):
         x = self.conv1(x)
-        x = nn.functional.relu(x) # use ReLU
+        x = nn.functional.relu(x)  # use ReLU
         x = self.maxpool(x)
         x = self.conv2(x)
         x = nn.functional.relu(x)
         x = self.maxpool(x)
-        x = x.flatten(start_dim=1) # flatten
-        x = self.lin1(x) 
+        x = x.flatten(start_dim=1)  # flatten
+        x = self.lin1(x)
         x = nn.functional.relu(x)
         x = self.out(x)
         x = nn.functional.log_softmax(x, dim=1)
         return x
-    
 
-class MINSTModel():
+
+class MINSTModel:
     def __init__(self) -> None:
         self.download_dataset()
 
     def download_dataset(self):
-        self.train_data = TV.datasets.MNIST("MNIST/", train=True, transform=None, target_transform=None, download=True)
-        self.test_data = TV.datasets.MNIST("MNIST/", train=False, transform=None, target_transform=None, download=True)
+        self.train_data = TV.datasets.MNIST(
+            "MNIST/", train=True, transform=None, target_transform=None, download=True
+        )
+        self.test_data = TV.datasets.MNIST(
+            "MNIST/", train=False, transform=None, target_transform=None, download=True
+        )
 
-        print('Number of samples in train_data is: ',len(self.train_data))
-        print('Number of samples in test_data is: ',len(self.test_data))    
+        print("Number of samples in train_data is: ", len(self.train_data))
+        print("Number of samples in test_data is: ", len(self.test_data))
 
-    def prepare_images(self, xt): # preprocess
+    def prepare_images(self, xt):  # preprocess
         out = torch.zeros(xt.shape)
         for i in range(xt.shape[0]):
             img = xt[i].unsqueeze(dim=0)
@@ -55,14 +60,14 @@ class MINSTModel():
         epochs = 100
         batch_size = 500
         lr = 1e-3
-        opt = torch.optim.Adam(params=self.model.parameters(),lr=lr)
+        opt = torch.optim.Adam(params=self.model.parameters(), lr=lr)
         lossfn = nn.NLLLoss()
 
         self.losses = []
         self.acc_CNN = []
 
         for i in range(epochs):
-            print('Current training epoch: ',i)
+            print("Current training epoch: ", i)
             opt.zero_grad()
             batch_ids_CNN = np.random.randint(0, 60000, size=batch_size)
             xt = self.train_data.data[batch_ids_CNN].detach()
@@ -71,28 +76,28 @@ class MINSTModel():
             pred = self.model(xt)
             pred_labels = torch.argmax(pred, dim=1)
             acc_ = 100.0 * (pred_labels == yt).sum() / batch_size
-            print('Current training accuracy: ', acc_.item())
+            print("Current training accuracy: ", acc_.item())
             self.acc_CNN.append(acc_)
             loss = lossfn(pred, yt)
-            print('Current training loss: ', loss.item())
-            self.losses.append(loss)    
+            print("Current training loss: ", loss.item())
+            self.losses.append(loss)
             loss.backward()
             opt.step()
-
 
     def test_model(self):
         test_id = np.random.randint(0, 10000, size=10)
 
         xt = self.test_data.data[test_id].detach()
-        xt =self.prepare_images(xt).unsqueeze(dim=1)
+        print(type(self.test_data.data[test_id]), type(xt))
+        xt = self.prepare_images(xt).unsqueeze(dim=1)
         preds = self.model(xt)
-        pred_ind = torch.argmax(preds.detach(), dim=1) 
+        pred_ind = torch.argmax(preds.detach(), dim=1)
         pred_ind = pred_ind.numpy()
 
-        for i in range (10):
-            x = self.test_data.data[test_id[i]]     
+        for i in range(10):
+            x = self.test_data.data[test_id[i]]
             plt.imshow(x)
-            print('\nThe number below is', pred_ind[i])  
+            print("\nThe number below is", pred_ind[i])
             plt.pause(0.05)
 
     def save_model(self):
@@ -103,9 +108,31 @@ class MINSTModel():
         self.model.load_state_dict(torch.load(PATH))
         self.model.eval()
 
-    def recognize_number(self):
-        # TODO:
-        pass
+    def recognize_number(self, image):
+        height, width = image.shape
+
+        dst = np.zeros((height, width), np.uint8)
+        for i in range(height):
+            for j in range(width):
+                dst[i, j] = 255 - image[i, j]
+        image = dst
+
+        import cv2
+
+        cv2.imwrite("./TRAINING_POWER_minst.png", image)
+
+        image = np.array(image).astype(np.float32)
+        image = np.expand_dims(image, 0)
+        image = np.expand_dims(image, 0)  # extend to [1，1，28，28]
+        image = torch.from_numpy(image)
+        # input = torch.from_numpy(image)
+        # input = self.prepare_images(input).unsqueeze(dim=1)
+        # print(type(input))
+        preds = self.model(image)
+        pred_ind = torch.argmax(preds.detach(), dim=1)
+        pred_ind = pred_ind.numpy()
+        print("\nThe number below is", pred_ind[0])
+
 
 if __name__ == "__main__":
     num_model = MINSTModel()
--- test/test_minst.py
+++ test/test_minst.py
@@ -1,9 +1,23 @@
+import cv2
+
 from script.minst import MINSTModel
+
 
 def test_load_model():
     minst_model = MINSTModel()
     minst_model.load_model()
     minst_model.test_model()
 
+
+def test_real_img():
+    minst_model = MINSTModel()
+    minst_model.load_model()
+    gray_image = cv2.imread("./equalized_thres_img4.png", 0)
+    # gray_image = cv2.resize(gray_image, (28, 28))
+    gray_image = gray_image[1:29, 1:29]
+    minst_model.recognize_number(gray_image)
+
+
 if __name__ == "__main__":
-    test_load_model()
+    # test_load_model()
+    test_real_img()
